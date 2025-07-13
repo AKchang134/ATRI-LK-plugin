672d21abde657fedfde175e4a5d5bb07dde4b240
l_o_o_k <2366360924@qq.com>
2025-07-13 09:56:45 +0800

    更新每日新闻插件

--- plugins/dailynews.py
+++ plugins/dailynews.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta, time
 from random import choice
 from typing import List
 import aiofiles
@@ -21,11 +22,11 @@ from ATRI.exceptions import str_traceback
 plugin = Service(
     "每日新闻",
     "每日新闻与摸鱼日历相关的服务",
-    "1.3.0",
+    "1.4.0",
     Service.ServiceType.FUNCTION
 )
 
-# url = "http://dwz.2xb.cn/zaob"
+old_url = "http://dwz.2xb.cn/zaob"
 url = 'https://api.southerly.top/api/60s?format=json'
 moyu = 'https://api.southerly.top/api/moyu'
 _lmt_notice = ["慢...慢一..点❤", "冷静1下", "歇会歇会~~", "呜呜...别急", "太快了...受不了", "不要这么快呀"]
@@ -47,7 +48,7 @@ today_news = plugin.on_command(cmd='今日新闻', docs="查看今日新闻")
 
 @today_news.handle([Cooldown(60 * 60, prompt=choice(_lmt_notice))])
 async def _():
-    _, news = await get_news()
+    _, news = await get_news(False)
     await today_news.finish(news)
 
 
@@ -105,18 +106,28 @@ async def daily_job():
                     await bot.send_group_msg(group_id=group_id, message=Message().append(mo_yu))
 
 
+async def send_daily_news():
+    _, message = await get_news()
+    for bot in get_bots().values():
+        if type(bot) is Bot:
+            group_list = await bot.get_group_list()
+            for group in group_list:
+                group_id = str(group["group_id"])
+                if group_id in config.groups:
+                    await bot.send_group_msg(group_id=group_id, message=Message().append(message))
+
+
 plugin.scheduler_jobs().add_job(daily_job, "新闻订阅", 'cron', hour=config.hour, minute=config.minute)
 
 
-async def get_news() -> tuple[bool, MessageSegment]:
+async def get_news(task: bool = True) -> tuple[bool, MessageSegment]:
     path = TEMP_DIR / "news.png"
     retry = 0
-    while retry < 5:
+    while retry < 3:
         try:
             resp = await request.get(url)
             resp.raise_for_status()
             url_json = resp.json()
-            # image_url = str(url_json["imageUrl"])
             image_url = str(url_json["data"]["image"])
             resp = await request.get(image_url)
             resp.raise_for_status()
@@ -127,6 +138,26 @@ async def get_news() -> tuple[bool, MessageSegment]:
         except Exception as e:
             log.warning(f"第{retry + 1}次获取新闻错误:\n{str_traceback(e)}")
             retry += 1
+    try:
+        resp = await request.get(old_url)
+        resp.raise_for_status()
+        url_json = resp.json()
+        image_url = str(url_json["imageUrl"])
+        date = datetime.strptime(url_json["datatime"], '%Y-%m-%d').date()
+        if task and date < get_now_date():
+            run_time = datetime.now() + timedelta(hours=4)
+            if not (time(hour=(config.hour - 4) % 24, minute=config.minute) <= run_time.time()
+                    <= time(hour=(config.hour + 4) % 24, minute=config.minute)):
+                plugin.scheduler_jobs().add_job(send_daily_news, "新闻订阅延时", 'date', run_date=run_time)
+            return False, MessageSegment.text(text="很遗憾，获取今日份新闻失败了捏")
+        resp = await request.get(image_url)
+        resp.raise_for_status()
+        content = resp.content
+        async with aiofiles.open(path, "wb") as wf:
+            await wf.write(content)
+        return True, MessageSegment.image(get_image_bytes(path))
+    except Exception as e:
+        log.warning(f"获取旧链接新闻错误:\n{str_traceback(e)}")
     return False, MessageSegment.text(text="很遗憾，获取今日份新闻失败了捏")
 
 
@@ -145,3 +176,12 @@ async def get_moyu() -> MessageSegment:
             log.warning(f"第{retry + 1}次获取摸鱼日历错误:\n{str_traceback(e)}")
             retry += 1
     return MessageSegment.text(text="很遗憾，今日份的摸鱼日历获取失败了捏")
+
+
+def get_now_date():
+    now = datetime.now()
+    if now.hour < 8:
+        custom_date = now - timedelta(days=1)
+    else:
+        custom_date = now
+    return custom_date.date()
